d4b3d244e3855a2f537e96e84c319a20852d9aa7
lorenzo.riches <lorenzo.riches@wfp.org>
2017-11-06 13:58:19 +0100

    solved divide by 0 problem in MAPE

--- Src/evaluation_utils.py
+++ Src/evaluation_utils.py
@@ -2,14 +2,21 @@ from scipy import stats
 from sklearn.metrics import make_scorer
 import numpy as np
 
+
 # SCORERS
 def r2_pearson(ground_truth, predictions):
     r2_pearson=stats.pearsonr(ground_truth, predictions)[0] ** 2
     return r2_pearson
 
+
 def MAPE(y, yhat):
-    diff = np.abs((y-yhat)/y)
+    if y != 0:
+        diff = np.abs((y-yhat)/y)
+    else:
+        diff = np.abs(yhat)
+
     return(np.sum(diff)/len(y))
 
+
 r2_pearson = make_scorer(r2_pearson, greater_is_better=True)
-MAPE  = make_scorer(MAPE, greater_is_better=False)
+MAPE = make_scorer(MAPE, greater_is_better=False)
