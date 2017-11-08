9996005b494f138e210f12d9d22e781be7c4db76
lorenzo.riches <lorenzo.riches@wfp.org>
2017-11-08 15:01:52 +0100

    first test of the score pictures script.

--- Src/utils.py
+++ Src/utils.py
@@ -42,3 +42,57 @@ def get_coordinates_from_shp(path_to_shape_file, spacing=1):
                 list_lon.append(lon)
                 list_lat.append(lat)
     return list_lat, list_lon
+
+def zonal_stats(path_to_shape_file, lon, lat, val):
+    """
+    given a shapefile and a list of coordinates and values, it returns the mean of the values for
+    each polygoon.
+    :param path_to_shape_file: string, path to shapefile
+    :param lon: list of longitudes
+    :param lat: list of latitudes
+    :param val: list of values
+    :return: list of means (one for each polygon)
+    """
+
+    def vals_in_poly(lon, lat, vals, pol):
+        v = []
+        for lo, la, val in zip(lon, lat, vals):
+            if pol.contains(Point(lo,la)):
+                v.append(val)
+        return np.mean(v)
+
+    # loop over the polygons
+    shp = shapefile.Reader(path_to_shape_file)
+    value_means = []
+    for poly in shp.shapes():
+        polygon = shape(poly)
+        value_means.append(vals_in_poly(lon, lat, val, polygon))
+    return value_means
+
+
+def shape2json(fname, outfile="states.json"):
+    """
+    function to convert a shapefile to GeoJSON.
+    Similar to: http://geospatialpython.com/2013/07/shapefile-to-geojson.html
+    :param fname: shp file path
+    :param outfile: json file path
+    """
+    # read the shapefile
+    reader = shapefile.Reader(fname)
+    fields = reader.fields[1:]
+    field_names = [field[0] for field in fields]
+    buffer = []
+    for sr in reader.shapeRecords():
+        atr = dict(zip(field_names, sr.record))
+        geom = sr.shape.__geo_interface__
+        buffer.append(dict(id=str(atr['ID_0'])+'_'+str(atr['ID_1'])+'_'+str(atr['ID_2']), type="Feature", \
+                           geometry=geom, properties=atr))
+
+    # write the GeoJSON file
+    from json import dumps
+    geojson = open(outfile, "w")
+    geojson.write(dumps({"type": "FeatureCollection", \
+                         "features": buffer}, indent=2) + "\n")
+    geojson.close()
+
+
--- scripts/score_pictures.py
+++ scripts/score_pictures.py
@@ -4,6 +4,15 @@
 - loads a pre-trained model
 - makes predictions
 """
+# ---------- #
+# PARAMETERS #
+config_id = 1
+adm0_file_path = "../Data/Shapefiles/UGA_adm_shp/UGA_adm0.shp"
+gpscoordinates_sampling = 1
+path_to_shapefile = "../Data/Shapefiles/UGA_adm_shp/UGA_adm2.shp"
+
+# --------- #
+# LIBRARIES #
 import os
 os.chdir('scripts')
 import sys
@@ -13,11 +22,15 @@ from sqlalchemy import create_engine
 import yaml
 import pandas as pd
 from nn_extractor import NNExtractor
-from utils import scoring_postprocess, get_coordinates_from_shp
+from utils import scoring_postprocess, get_coordinates_from_shp, zonal_stats, shape2json
+from sklearn.externals import joblib
+import json
+import numpy as np
+from branca.colormap import linear
+import folium
 
-# --------------- #
-# SETUP ###########
-# load config
+# ------#
+# SETUP #
 with open('../private_config.yml', 'r') as cfgfile:
     private_config = yaml.load(cfgfile)
 
@@ -26,46 +39,74 @@ engine = create_engine("""postgresql+psycopg2://{}:{}@{}/{}"""
                        .format(private_config['DB']['user'], private_config['DB']['password'],
                                private_config['DB']['host'], private_config['DB']['database']))
 
-config = pd.read_sql_query("select * from config where id = {}".format(1), engine)
-# TODO
-image_dir = os.path.join("../Data", "Satellite", "test")
+config = pd.read_sql_query("select * from config where id = {}".format(config_id), engine)
 
 # get raster
+image_dir = os.path.join("../Data", "Satellite", config["satellite_source"][0])
 GRID = RasterGrid(config["satellite_grid"][0], image_dir)
 
-# ---------------------- #
-# IMAGES IN SCOPE ########
-# ADM0
-adm0_lat, adm0_lon = get_coordinates_from_shp("../Data/Shapefiles/UGA_adm_shp/UGA_adm0.shp", spacing=0.5)
+# ----------------#
+# IMAGES IN SCOPE #
+print("INFO: downloading images in scope ...")
+adm0_lat, adm0_lon = get_coordinates_from_shp(adm0_file_path, spacing=gpscoordinates_sampling)
 # get the matching tiles
 list_i, list_j = GRID.get_gridcoordinates2(adm0_lat, adm0_lon)
 # download images to predict
-GRID.download_images(list_i, list_j, step=0, provider='Google')
-# extract features
-network = NNExtractor("""..\Data\Satellite\\test""", config['network_model'][0], step=0)
-features = scoring_postprocess(network.extract_features())
+GRID.download_images(list_i, list_j, step=config['satellite_step'][0], provider=config['satellite_source'][0])
+
+# ---------------------------------- #
+# EXTRACT FEATURES FOR IMGS IN SCOPE #
+print("INFO: extracting features for images in scope ...")
+network = NNExtractor(image_dir, config['network_model'][0], step=config['satellite_step'][0])
+features = scoring_postprocess(network.extract_features(list_i, list_j))
+
+# ---------------------- #
+# LOAD MODEL AND PREDICT #
+print("INFO: computing means for lower adm level ...")
 X = features.drop(['index', 'i', 'j'], axis=1)
-
 # load model and predict
-from sklearn.externals import joblib
-clf = joblib.load('../Models/ridge_model_config_id_1.pkl')
-yhat = clf.predict(X)
+try:
+    clf = joblib.load('../Models/ridge_model_config_id_{}.pkl'.format(config_id))
+except FileNotFoundError:
+    print('ERROR: model not found')
 
-# make simple plot
-import folium
+yhat = clf.predict(X)
+results = pd.DataFrame({'lat': adm0_lat, 'lon': adm0_lon, 'yhat': yhat})
+
+# ---------------------- #
+# COMPUTE MEANS FOR ADM2 #
+print("INFO: computing means for lower adm level ...")
+polygon_means = zonal_stats(path_to_shapefile, results['lon'], results['lat'], results['yhat'])
+
+# ---- #
+# PLOT #
+# convert shapefile to GeoJSON
+print("INFO: printing ...")
+shape2json(path_to_shapefile, outfile=path_to_shapefile[:-3]+"json")
+
+with open(path_to_shapefile[:-3]+"json") as f:
+    adm2_map = json.load(f)
+
+map_df = pd.DataFrame({'loc': [adm2_map['features'][i]['id'] for i in range(0,len(adm2_map['features']))],
+                       'means': polygon_means})
+
+map_df.means = map_df.means.fillna(np.mean(map_df.means))  # TODO: handle nans
+
+colormap = linear.YlGn.scale(
+    map_df.means.min(),
+    map_df.means.max())
+
 m = folium.Map(
     location=[1.130956, 32.354771],
     tiles='Stamen Terrain',
     zoom_start=6
 )
+m.choropleth(
+    geo_data=path_to_shapefile[:-3]+"json",
+    data=map_df,
+    columns=['loc', 'means'],
+    key_on='feature.id',
+    fill_color='YlGn',
+)
 
-for i in range(0, len(yhat)-1):
-    folium.CircleMarker(
-        location=[uga_lat[i], uga_lon[i]],
-        radius=yhat[i]*10,
-        popup=str(yhat[i]),
-        color='#3186cc',
-        fill=True,
-        fill_color='#3186cc'
-    ).add_to(m)
-m.save('map.html')
+m.save('map_config_{}.html'.format(config_id))
