183d49fbd270314b83d93ded4f9667535dd74595
lorenzo.riches <lorenzo.riches@wfp.org>
2017-11-09 10:07:34 +0100

    added option via click.

--- scripts/score_pictures.py
+++ scripts/score_pictures.py
@@ -1,21 +1,11 @@
+# -*- coding: utf-8 -*-
 """
 - downloads the pictures relevant for scoring
 - extracts features
 - loads a pre-trained model
 - makes predictions
 """
-# ---------- #
-# PARAMETERS #
-config_id = 1
-adm0_file_path = "../Data/Shapefiles/UGA_adm_shp/UGA_adm0.shp"
-gpscoordinates_sampling = 1
-adm = 1
-path_to_shapefile = "../Data/Shapefiles/UGA_adm_shp/UGA_adm1.shp"
-
-# --------- #
-# LIBRARIES #
 import os
-os.chdir('scripts')
 import sys
 sys.path.append(os.path.join("..", "Src"))
 from img_lib import RasterGrid
@@ -28,90 +18,107 @@ from sklearn.externals import joblib
 import json
 import numpy as np
 from branca.colormap import linear
+import click
 import folium
 
-# ------#
-# SETUP #
-with open('../private_config.yml', 'r') as cfgfile:
-    private_config = yaml.load(cfgfile)
 
-# connect to db and read config table
-engine = create_engine("""postgresql+psycopg2://{}:{}@{}/{}"""
-                       .format(private_config['DB']['user'], private_config['DB']['password'],
-                               private_config['DB']['host'], private_config['DB']['database']))
+# ---------- #
+# PARAMETERS #
+@click.command()
+@click.option('--adm0_file_path', type=click.Path(exists=True), default="../Data/Shapefiles/UGA_adm_shp/UGA_adm0.shp")
+@click.option('--path_to_shapefile', type=click.Path(exists=True), default="../Data/Shapefiles/UGA_adm_shp/UGA_adm1.shp")
+@click.option('--config_id', default=1)
+@click.option('--gpscoordinates_sampling', default=0.5)
+@click.option('--adm', default=1)
+def main(adm0_file_path, path_to_shapefile, config_id, gpscoordinates_sampling, adm):
 
-config = pd.read_sql_query("select * from config where id = {}".format(config_id), engine)
+    # ------#
+    # SETUP #
+    with open('../private_config.yml', 'r') as cfgfile:
+        private_config = yaml.load(cfgfile)
 
-# get raster
-image_dir = os.path.join("../Data", "Satellite", config["satellite_source"][0])
-GRID = RasterGrid(config["satellite_grid"][0], image_dir)
+    # connect to db and read config table
+    engine = create_engine("""postgresql+psycopg2://{}:{}@{}/{}"""
+                           .format(private_config['DB']['user'], private_config['DB']['password'],
+                                   private_config['DB']['host'], private_config['DB']['database']))
 
-# ----------------#
-# IMAGES IN SCOPE #
-print("INFO: downloading images in scope ...")
-adm0_lat, adm0_lon = get_coordinates_from_shp(adm0_file_path, spacing=gpscoordinates_sampling)
-# get the matching tiles
-list_i, list_j = GRID.get_gridcoordinates2(adm0_lat, adm0_lon)
-# download images to predict
-GRID.download_images(list_i, list_j, step=config['satellite_step'][0], provider=config['satellite_source'][0])
+    config = pd.read_sql_query("select * from config where id = {}".format(config_id), engine)
 
-# ---------------------------------- #
-# EXTRACT FEATURES FOR IMGS IN SCOPE #
-print("INFO: extracting features for images in scope ...")
-network = NNExtractor(image_dir, config['network_model'][0], step=config['satellite_step'][0])
-features = scoring_postprocess(network.extract_features(list_i, list_j))
+    # get raster
+    image_dir = os.path.join("../Data", "Satellite", config["satellite_source"][0])
+    GRID = RasterGrid(config["satellite_grid"][0], image_dir)
 
-# ---------------------- #
-# LOAD MODEL AND PREDICT #
-print("INFO: computing means for lower adm level ...")
-X = features.drop(['index', 'i', 'j'], axis=1)
-# load model and predict
-try:
-    clf = joblib.load('../Models/ridge_model_config_id_{}.pkl'.format(config_id))
-except FileNotFoundError:
-    print('ERROR: model not found')
+    # ----------------#
+    # IMAGES IN SCOPE #
+    print("INFO: downloading images in scope ...")
+    adm0_lat, adm0_lon = get_coordinates_from_shp(adm0_file_path, spacing=gpscoordinates_sampling)
+    # get the matching tiles
+    list_i, list_j = GRID.get_gridcoordinates2(adm0_lat, adm0_lon)
+    # download images to predict
+    GRID.download_images(list_i, list_j, step=config['satellite_step'][0], provider=config['satellite_source'][0])
 
-yhat = clf.predict(X)
-results = pd.DataFrame({'lat': adm0_lat, 'lon': adm0_lon, 'yhat': yhat})
+    # ---------------------------------- #
+    # EXTRACT FEATURES FOR IMGS IN SCOPE #
+    print("INFO: extracting features for images in scope ...")
+    network = NNExtractor(image_dir, config['network_model'][0], step=config['satellite_step'][0])
+    features = scoring_postprocess(network.extract_features(list_i, list_j))
 
-# ---------------------- #
-# COMPUTE MEANS FOR ADM2 #
-print("INFO: computing means for lower adm level ...")
-polygon_means = zonal_stats(path_to_shapefile, results['lon'], results['lat'], results['yhat'])
+    # ---------------------- #
+    # LOAD MODEL AND PREDICT #
+    print("INFO: load model and predict ...")
+    X = features.drop(['index', 'i', 'j'], axis=1)
+    # load model and predict
+    try:
+        clf = joblib.load('../Models/ridge_model_config_id_{}.pkl'.format(config_id))
+    except FileNotFoundError:
+        print('ERROR: model not found')
 
-# ---- #
-# PLOT #
-# convert shapefile to GeoJSON
-print("INFO: printing ...")
-shape2json(path_to_shapefile, outfile=path_to_shapefile[:-3]+"json")
+    yhat = clf.predict(X)
+    results = pd.DataFrame({'lat': adm0_lat, 'lon': adm0_lon, 'yhat': yhat})
 
-with open(path_to_shapefile[:-3]+"json") as f:
-    adm2_map = json.load(f)
+    # ---------------------- #
+    # COMPUTE MEANS FOR ADM2 #
+    print("INFO: computing means for lower adm level ...")
+    polygon_means = zonal_stats(path_to_shapefile, results['lon'], results['lat'], results['yhat'])
 
-map_df = pd.DataFrame({'loc': [adm2_map['features'][i]['id'] for i in range(0,len(adm2_map['features']))],
-                       'means': polygon_means})
+    # ---- #
+    # PLOT #
+    # convert shapefile to GeoJSON
+    print("INFO: printing ...")
+    shape2json(path_to_shapefile, outfile=path_to_shapefile[:-3]+"json")
 
-map_df.means = map_df.means.fillna(np.mean(map_df.means))  # TODO: handle nans
+    with open(path_to_shapefile[:-3]+"json") as f:
+        adm2_map = json.load(f)
 
-colormap = linear.YlGn.scale(
-    map_df.means.min(),
-    map_df.means.max())
+    map_df = pd.DataFrame({'loc': [adm2_map['features'][i]['id'] for i in range(0,len(adm2_map['features']))],
+                           'means': polygon_means})
 
-m = folium.Map(
-    location=[1.130956, 32.354771],
-    tiles='MapQuest Open Aerial',
-    zoom_start=6
-)
-m.choropleth(
-    geo_data=path_to_shapefile[:-3]+"json",
-    data=map_df,
-    columns=['loc', 'means'],
-    key_on='feature.id',
-    fill_color='YlGn',
-)
+    map_df.means = map_df.means.fillna(np.mean(map_df.means))  # TODO: handle nans
 
-# --------- #
-# SAVE PLOT #
-if not os.path.exists('../Plots'):
-    os.makedirs('../Plots')
-    m.save(os.path.join('../Plots','map_config_{}_amd_{}.html'.format(config_id, adm)))
+    colormap = linear.YlGn.scale(
+        map_df.means.min(),
+        map_df.means.max())
+
+    m = folium.Map(
+        location=[1.130956, 32.354771],
+        tiles='MapQuest Open Aerial',
+        zoom_start=6
+    )
+    m.choropleth(
+        geo_data=path_to_shapefile[:-3]+"json",
+        data=map_df,
+        columns=['loc', 'means'],
+        key_on='feature.id',
+        fill_color='YlGn',
+    )
+
+    # --------- #
+    # SAVE PLOT #
+    if not os.path.exists('../Plots'):
+        os.makedirs('../Plots')
+        m.save(os.path.join('../Plots','map_config_{}_amd_{}.html'.format(config_id, adm)))
+
+
+if __name__ == '__main__':
+
+    main()
