b27c7230da03491c72b0c578b429ad76cdf416ee
lorenzo.riches <lorenzo.riches@wfp.org>
2017-12-20 14:08:46 +0100

    trianing history

--- scripts/fine_tuning.py
+++ scripts/fine_tuning.py
@@ -28,7 +28,7 @@ img_width, img_height = 400, 400
 nb_train_samples = 489
 nb_validation_samples = 273
 epochs = 50
-batch_size = 8
+batch_size = 16
 
 # build the VGG16 network
 base_model = tf.keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=(400,400,3))
@@ -77,9 +77,27 @@ validation_generator = test_datagen.flow_from_directory(
     class_mode='categorical')
 
 # fine-tune the model
-model.fit_generator(
+history = model.fit_generator(
     train_generator,
     steps_per_epoch=nb_train_samples // batch_size,
     epochs=epochs,
     validation_data=validation_generator,
     validation_steps=nb_validation_samples // batch_size)
+
+# plot training
+import matplotlib as plt
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r.')
+plt.plot(epochs, val_acc, 'r')
+plt.title('Training and validation accuracy')
+
+plt.figure()
+plt.plot(epochs, loss, 'r.')
+plt.plot(epochs, val_loss, 'r-')
+plt.title('Training and validation loss')
+plt.save('training_history.png')
