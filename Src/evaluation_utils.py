58e78c0c72d6134f52ac3d19980c8e34f33d21c1
lorenzo.riches <lorenzo.riches@wfp.org>
2018-01-16 17:21:31 +0100

    train with r2, score on r2, r2pears, MAPE

--- Src/evaluation_utils.py
+++ Src/evaluation_utils.py
@@ -5,8 +5,13 @@ import numpy as np
 
 # SCORERS
 def r2_pearson(ground_truth, predictions):
-    r2_pearson=stats.pearsonr(ground_truth, predictions)[0] ** 2
+    r2_pearson = abs(stats.pearsonr(ground_truth, predictions)[0] ** 2)
     return r2_pearson
+
+
+def R2(y_true, y_pred):
+    r2 = abs(r2_score(y_true, y_pred))
+    return r2
 
 
 def MAPE(y, yhat):
@@ -16,6 +21,6 @@ def MAPE(y, yhat):
     return(np.sum(diff)/len(y))
 
 
-r2 = make_scorer(r2_score, greater_is_better=True) # counter intuitive but otherwise shit results???
+r2 = make_scorer(R2, greater_is_better=True) # counter intuitive but otherwise shit results???
 r2_pearson = make_scorer(r2_pearson, greater_is_better=True)
 MAPE = make_scorer(MAPE, greater_is_better=False)
