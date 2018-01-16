c322c38a5117d39aded2bdc581d4ff163bebcfb1
Lorenzo Riches <lo.riches@gmail.com>
2018-01-16 16:45:40 +0000

    no idea what is going on

--- Src/evaluation_utils.py
+++ Src/evaluation_utils.py
@@ -5,12 +5,12 @@ import numpy as np
 
 # SCORERS
 def r2_pearson(ground_truth, predictions):
-    r2_pearson = abs(stats.pearsonr(ground_truth, predictions)[0] ** 2)
+    r2_pearson = stats.pearsonr(ground_truth, predictions)[0] ** 2
     return r2_pearson
 
 
 def R2(y_true, y_pred):
-    r2 = abs(r2_score(y_true, y_pred))
+    r2 = r2_score(y_true, y_pred)
     return r2
 
 
