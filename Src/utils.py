2a464666a5afc718b63d5da94b9e8f57d2c5b9b6
lorenzo.riches <lorenzo.riches@wfp.org>
2018-10-24 09:24:35 +0200

    using S3 for the models.

--- Src/utils.py
+++ Src/utils.py
@@ -280,11 +280,11 @@ def s3_download(bucket, file, dest):
     """ Given a bucket name, file and destination filepath it downlaods the file from S3"""
     import boto3
     import os
-    boto3.client(
-            's3',
-            aws_access_key_id=os.environ['aws_access_key_id'],
-            aws_secret_access_key=os.environ['aws_secret_access_key']
-        )
+    boto3.Session(
+        aws_access_key_id=os.environ['aws_access_key_id'],
+        aws_secret_access_key=os.environ['aws_secret_access_key'],
+        region_name='eu-central-1', )
+
     s3 = boto3.resource('s3')
     s3.Bucket(bucket).download_file(file, dest)
     print("INFO: file downloaded to ", dest)
